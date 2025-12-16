34b1a356271c2de13c988c78fe04aef80dfcef17
Devesh Krishna Mishra <mdevesh47@gmail.com>
2025-12-16 17:12:07 +0530

    update resume url

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -28,7 +28,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   const resumeUrl =
-    "https://drive.google.com/file/d/1GgISleupeeBcTW8ELdBTWUYvV2XLamzd/view?usp=sharing";
+    "https://drive.google.com/file/d/1I7p6xhBBVuAKtyZ-6JSVraoD9fKoTK-9/view?usp=sharing";
 
   return (
     <html lang="en">
